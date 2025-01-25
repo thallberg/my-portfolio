ee6a681e74bb408edec06ced31935095491ac42b
thallberg <T.hallberg84@gmail.com>
2025-01-25 12:11:00 +0100

    changed name in contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
-import style from "./contact.module.css"
+import style from "./Contact.module.css"
 
 const Contact = () => {
   return (
