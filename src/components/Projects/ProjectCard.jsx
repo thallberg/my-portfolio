94ef5f814812ca0788c573177fae8e2cb4487685
thallberg <T.hallberg84@gmail.com>
2025-01-25 18:14:17 +0100

    added members in the projects.json file

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { getImageUrl } from "../../utils";
 import style from "./ProjectCard.module.css";
 
 const ProjectCard = ({
-  project: { title, description, skills, demo, source, imageSrc },
+  project: { title, description, member, demo, source, imageSrc },
 }) => {
   return (
     <div className={style.container}>
@@ -14,20 +14,40 @@ const ProjectCard = ({
       />
       <h3 className={style.title}>{title}</h3>
       <p className={style.description}>{description}</p>
-      <ul className={style.skills}>
-        {skills.map((skill, id) => {
-          <li key={id} className={style.skill}>
-            {skill}
-          </li>;
-        })}
+      <ul className={style.members}>
+        {member.map((person, id) => (
+          <li key={id} className={style.member}>
+            <a
+              href={person.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={style.githubLink}
+            >
+              <img
+                src={getImageUrl("./assets/contact/githubIcon.png")}
+                alt="Github icon"
+                className={style.githubIcon}
+              />
+              {`${person.name}'s GitHub`}
+            </a>
+          </li>
+        ))}
       </ul>
       <div className={style.links}>
-        <a className={style.link} href={demo}
-        target="_blank">
+        <a
+          className={style.link}
+          href={demo}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Demo
         </a>
-        <a className={style.link} href={source}
-        target="_blank">
+        <a
+          className={style.link}
+          href={source}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Source
         </a>
       </div>
